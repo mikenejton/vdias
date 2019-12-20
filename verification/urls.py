97cc5a6afec1b1aa8d7f6e1ac0bd020ebffcf6ba
Nejton <7650700@mail.ru>
2019-12-20 14:18:00 +0300

    front fixing

--- verification/urls.py
+++ verification/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('/find-vitem', views.find_vitem, name='find-vitem')
+    path('find-vitem', views.find_vitem, name='find-vitem')
 ]
